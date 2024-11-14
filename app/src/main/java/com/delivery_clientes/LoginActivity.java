cbfb454a46c8317955053a18824c895f91f9e3ae
LuisFIbarra <luis.f.ibarra.93@gmail.com>
2024-11-14 01:29:24 -0300

    Small Bugfix a ciertos constraints + comportamiento onBackPressed()

--- app/src/main/java/com/delivery_clientes/LoginActivity.java
+++ app/src/main/java/com/delivery_clientes/LoginActivity.java
@@ -7,8 +7,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
 import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.delivery_clientes.ui.home.HomeFragment;
+import com.delivery_clientes.ui.login.LoginFragment;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -16,5 +22,25 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    @Override
+    public void onBackPressed() {
+        // Obtiene el NavHostFragment
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment);
+        if (navHostFragment != null) {
+            NavController navController = navHostFragment.getNavController();
+            NavDestination currentDestination = navController.getCurrentDestination();
+
+            // Verifica si el destino actual es HomeFragment
+            if (currentDestination != null && currentDestination.getId() == R.id.homeFragment) {
+                moveTaskToBack(true);
+            } else {
+                super.onBackPressed();
+            }
+        } else {
+            super.onBackPressed();
+        }
     }
 }
--- app/src/main/java/com/delivery_clientes/ui/home/HomeFragment.java
+++ app/src/main/java/com/delivery_clientes/ui/home/HomeFragment.java
@@ -4,10 +4,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowInsets;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.view.WindowCompat;
 import androidx.fragment.app.Fragment;
+
+import androidx.core.view.WindowInsetsControllerCompat;
 
 import com.delivery_clientes.R;
 
@@ -19,5 +23,18 @@ public class HomeFragment extends Fragment {
                              @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.setOnApplyWindowInsetsListener((v, insets) -> {
+            v.setPadding(0, 0, 0, insets.getSystemWindowInsetBottom());
+            return insets;
+        });
+    }
+
+
+
 
 }
