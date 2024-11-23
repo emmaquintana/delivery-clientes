b9a2bcfeabed9865543e1c528e0aac7110ade9ca
Emmanuel Quintana <eqb2021@gmail.com>
2024-11-23 04:33:08 -0300

    agrega el metodo obtenerUsuarios

--- app/src/main/java/com/delivery_clientes/data/db/dao/UsuariosDAO.java
+++ app/src/main/java/com/delivery_clientes/data/db/dao/UsuariosDAO.java
@@ -6,6 +6,8 @@ import androidx.room.Query;
 
 import com.delivery_clientes.data.db.entities.Usuario;
 
+import java.util.List;
+
 @Dao
 public interface UsuariosDAO {
 
@@ -17,4 +19,7 @@ public interface UsuariosDAO {
 
     @Query("SELECT * FROM usuarios WHERE email = :email")
     Usuario findUserByUsername(String email);
+
+    @Query("SELECT * FROM usuarios")
+    List<Usuario> obtenerUsuarios();
 }
